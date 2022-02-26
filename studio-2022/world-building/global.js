e0b9294ae0b9b3640e30be7bc4ab74dc9a00a67b
lqkii <90734839+lqkii@users.noreply.github.com>
2022-02-25 19:43:53 -0500

    asdlkjasdkjas

---
+++ studio-2022/world-building/global.js
@@ -0,0 +1,14 @@
+// element.onscroll = function() {
+
+// }
+
+
+
+// window.addEventListener('scroll', pageChange)
+
+function pageChange(i) {
+    // document.location.href = newUrl;
+    console.log("hello");
+}
+
+console.log("hello");
