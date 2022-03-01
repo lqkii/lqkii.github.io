b7c79082acaf6b4e658c4924df2958a085404b46
lqkii <90734839+lqkii@users.noreply.github.com>
2022-03-01 16:50:16 -0500

    world building

--- studio-2022/world-building/global.js
+++ studio-2022/world-building/global.js
@@ -1,14 +1,10 @@
-// element.onscroll = function() {
+// When the page scrolls...
+window.addEventListener("scroll", function(e) {
+ 
+// What % down is it? 
+// https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+// Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-// }
-
-
-
-// window.addEventListener('scroll', pageChange)
-
-function pageChange(i) {
-    // document.location.href = newUrl;
-    console.log("hello");
-}
-
-console.log("hello");
+console.log(scrollPercentage)
+});
