c182e2e6a2733eb676a699b3bba52fbd8d101890
lqkii <90734839+lqkii@users.noreply.github.com>
2022-03-08 01:50:54 -0500

    working

--- studio-2022/world-building/global.js
+++ studio-2022/world-building/global.js
@@ -1,20 +1,47 @@
-var rangeslider = document.getElementById("slider");
+var rangeSlider = document.getElementById("slider");
+var lastNum = 0;
+var increase = 0;
 
-rangeslider.oninput = function () {
-  console.log(rangeslider.value);
-    if(rangeslider.value <= 20) {
-      console.log("swag");
+// check increase/decrease
+rangeSlider.oninput = function() {
+    if(lastNum < rangeSlider.value) {
+        console.log("increasing");
+        increase = 1;
+        console.log(increase);
+    }   else {
+            console.log("decreasing");
+            increase = 0;
+            console.log(increase);
+        }
+    lastNum = rangeSlider.value;
+    console.log("onInput: " + rangeSlider.value);
+
+// numb <-> fantasy love
+    
+    if(rangeSlider.value == 20 && increase == 1) {
+    // console.log("swag2");
+    location.href = "/studio-2022/world-building/pages/fantasy-love.html";
+    }  
+
+    if(rangeSlider.value == 20 && increase == 0) {
+    //   console.log("swag");
       location.href = "../index.html";
     }
 
-    if(rangeslider.value >= 60) {
-    console.log("swag2");
+// fantasy love <-> over the moon
+
+    if(rangeSlider.value == 40 && increase == 1) {
+    // console.log("swag2");
+    location.href = "/studio-2022/world-building/pages/over-the-moon.html";
+    }  
+
+    if(rangeSlider.value == 40 && increase == 0) {
+    //   console.log("swag");
     location.href = "/studio-2022/world-building/pages/fantasy-love.html";
     }
-};
+    
+}
 
-// if value is above __ then do __, if not do nothing
+// if increase/decrease variable is true, and rangeslider.value = __, then execute __
 
-    // if value was below __, and is now above __, then __
-    // if value was above __, and is now below __, then __
-    // if value increased/decreased __
+// the faster you swipe, the more numbers it skips, probably do a range of values like +-10?
