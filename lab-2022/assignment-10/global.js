bc7023691cf52689f00134a6f2177721a53802b4
lqkii <90734839+lqkii@users.noreply.github.com>
2022-04-22 05:55:48 -0400

    bongy dongy

--- lab-2022/assignment-10/global.js
+++ lab-2022/assignment-10/global.js
@@ -3,6 +3,8 @@
 let mouseCursor = document.querySelector(".cursor");
 window.addEventListener("mousemove", cursor);
 
+
+// Cursor load in & follow mouse position
 function cursor(e) {
     mouseCursor.style.top = e.pageY + "px";
     mouseCursor.style.left = e.pageX + "px";
@@ -10,15 +12,18 @@ function cursor(e) {
     
 }
 
+// Declaring variables
 let hoverText = document.querySelector(".title");
 let line = document.querySelector(".line");
 let number = document.querySelector(".number");
 let test = document.querySelector(".test");
 let invert = document.querySelector(".invert")
 
+// Event listeners for hover & mouse leaving functions
 hoverText.addEventListener("mouseover", hoverAnim)
 hoverText.addEventListener("mouseleave", leaveAnim)
 
+// On hover functions
 function hoverAnim() {
     hoverText.classList.add("title-hover")
     line.classList.add("line-hover")
@@ -29,6 +34,7 @@ function hoverAnim() {
     // console.log("cool")
 }
 
+// Mouse leaving functions
 function leaveAnim() {
     hoverText.classList.remove("title-hover")
     line.classList.remove("line-hover")
